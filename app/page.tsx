17a940c9fad91ef780fb0506c9cc050801f7f3e5
Brendan-Doy1e <brendan-doyle@hotmail.com>
2024-06-18 17:28:50 -0700

    Added more customization and about me section

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,109 @@
+"use client";
+
 import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+    <main className="flex min-h-screen items-center justify-center p-24 bg-cover bg-center" style={{ backgroundImage: 'url(/path/to/your/background-image.jpg)' }}>
+      <div className="flex w-full max-w-7xl lg:flex-row flex-col items-center justify-center">
+        
+        <div className="flex-1 mb-32 lg:mb-0 lg:w-2/3 lg:text-left text-center flex flex-col justify-center">
+          <div>
+            <TypeAnimation
+              sequence={['public String welcome() {']}
+              wrapper="h2"
+              cursor={false}
+              repeat={0}
+              className="mb-3 text-3xl font-semibold text-white text-shadow"
+            />
+            <TypeAnimation
+              sequence={[
+                1200, 
+                '        return "Hello world, I\'m Brendan!";'.replace(/ /g, '\u00a0'), // 8 non-breaking spaces for 2 tabs
+              ]}
+              wrapper="h2"
+              cursor={false}
+              repeat={0}
+              className="mb-3 text-3xl font-semibold text-white text-shadow"
+            />
+            <TypeAnimation
+              sequence={[
+                4200, 
+                '}',
+              ]}
+              wrapper="h2"
+              cursor={true}
+              repeat={0}
+              className="mb-3 text-3xl font-semibold text-white text-shadow"
+            />
+          </div>
+          <hr className="my-4 border-t-2 border-gray-300 opacity-50" />
+          <p className="max-w-[80ch] text-lg leading-relaxed text-white text-shadow">
+            I am a recent graduate of computer science at BCIT. I am actively seeking software development positions where I can grow my technical skills and passion for innovation.
+          <br /><br />
+            I am an avid golfer and extreme sports enthusiast. I hold a license in skydiving, advanced scuba diving, certified ski instructor and represented the national team in skeleton sliding.
+          </p>
         </div>
-      </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://www.linkedin.com/in/brendan-j-doyle/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            LinkedIn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find out more about me on LinkedIn.
-          </p>
-        </a>
 
-        <a
-          href="https://www.github.com/Brendan-Doy1e"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Github{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            See my projects on Github.
-          </p>
-        </a>
+        <div className="flex-1 mb-32 lg:mb-0 lg:w-1/3 lg:text-left text-center flex flex-col space-y-4 justify-center">
+          <a
+            href="https://www.linkedin.com/in/brendan-j-doyle/"
+            className="group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex items-center justify-center w-2/3 mx-auto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="text-center text-white text-shadow">
+              <h2 className="mb-3 text-2xl font-semibold">
+                LinkedIn{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Find out more about me on LinkedIn.
+              </p>
+            </div>
+          </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            My Projects{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Check out my projects.
-          </p>
-        </a>
+          <a
+            href="https://www.github.com/Brendan-Doy1e"
+            className="group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex items-center justify-center w-2/3 mx-auto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="text-center text-white text-shadow">
+              <h2 className="mb-3 text-2xl font-semibold">
+                Github{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                See my projects on Github.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="group rounded-lg border border-transparent px-4 py-3 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 flex items-center justify-center w-2/3 mx-auto"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="text-center text-white text-shadow">
+              <h2 className="mb-3 text-2xl font-semibold">
+                My Projects{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-2 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+              <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                Check out my projects.
+              </p>
+            </div>
+          </a>
+        </div>
       </div>
     </main>
   );
